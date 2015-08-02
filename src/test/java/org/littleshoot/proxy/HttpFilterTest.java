43e36e40675433a7398b3b3b4851e41ca03d69ad
Jason Eric Klaes Hoetger <jekh@outlook.com>
2015-08-02 14:53:48 -0700

    Added sleep statements to allow filters to be invoked after making HTTP requests

--- src/test/java/org/littleshoot/proxy/HttpFilterTest.java
+++ src/test/java/org/littleshoot/proxy/HttpFilterTest.java
@@ -449,6 +449,7 @@ public class HttpFilterTest {
         setUpHttpProxyServer(filtersSource);
 
         getResponse("http://localhost:" + webServerPort + "/");
+        Thread.sleep(500);
 
         assertTrue("proxyToServerResolutionSucceeded method was not called", resolutionSucceeded.get());
     }
@@ -474,6 +475,7 @@ public class HttpFilterTest {
                 .start();
 
         getResponse("http://www.doesnotexist/some-resource");
+        Thread.sleep(500);
 
         // verify that the filters related to this functionality were correctly invoked/not invoked as appropriate, but also verify that
         // other filters were invoked/not invoked as expected
@@ -513,6 +515,7 @@ public class HttpFilterTest {
 
         // port 0 is not connectable
         getResponse("http://localhost:0/some-resource");
+        Thread.sleep(500);
 
         // verify that the filters related to this functionality were correctly invoked/not invoked as appropriate, but also verify that
         // other filters were invoked/not invoked as expected
@@ -705,6 +708,8 @@ public class HttpFilterTest {
         org.apache.http.HttpResponse httpResponse = getResponse("http://localhost:" + mockServerPort + "/servertimeout");
         assertEquals("Expected to receive an HTTP 504 Gateway Timeout from proxy", 504, httpResponse.getStatusLine().getStatusCode());
 
+        Thread.sleep(500);
+        
         // verify that the filters related to this functionality were correctly invoked/not invoked as appropriate, but also verify that
         // other filters were invoked/not invoked as expected
         assertTrue("Expected filter method to be called", filter.isServerToProxyResponseTimedOutInvoked());
@@ -764,6 +769,7 @@ public class HttpFilterTest {
 
         // test with a POST request with a payload. post a large amount of data, to force chunked content.
         postToServer("http://localhost:" + webServerPort + "/", 50000);
+        Thread.sleep(500);
 
         assertTrue("proxyToServerRequest callback was not invoked for LastHttpContent for chunked POST", lastHttpContentProcessed.get());
         assertTrue("proxyToServerRequestSent callback was not invoked for chunked POST", requestSentCallbackInvoked.get());
